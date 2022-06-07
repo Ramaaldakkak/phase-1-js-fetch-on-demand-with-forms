db663dde25928d1b08618adc828a8f71a136bcc4
Rama Aldakkak <rama.aldakkak@gmail.com>
2022-06-07 12:06:01 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const init = () => {
-  
+
+    // Add event listeners to capture form data and override a form's default behavior
+    const inputForm = document.querySelector('form')
+    inputForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const input = document.querySelector('input#searchByID');
+    console.log(input.value)
+
+    // Fetch data based on what the user types into that form
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(response => response.json())
+    .then(data => {
+
+    // Display that data on the page
+    const title = document.querySelector('section#movieDetails h4')
+    const summary = document.querySelector('section#movieDetails p')
+
+    title.innerText = data.title
+    summary.innerText = data.summary
+    })
+})
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', init)
